0397086a155406898f16f4d19438819162323260
salwa essoussi <salwa.ess@gmail.com>
2016-07-04 16:30:59 -0400

    Created back-end and setted it up

---
+++ controllers/api_controller.js
@@ -0,0 +1,20 @@
+// const router         = require('express').Router();
+
+// const dbWeather      = require ('../models/weather')
+// const dbSubway       = require('../models/subway')
+
+// router.get('/', function(req,res) {
+//   res.json({"work":"it worked!"});
+// });
+
+
+// router.get('/weather', function(req,res) {
+//   res.json();
+// });
+
+// router.get('/subway', function(req,res) {
+//   res.json();
+// });
+
+
+// module.exports = router;
